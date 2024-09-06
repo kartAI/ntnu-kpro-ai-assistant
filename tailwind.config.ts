6f970dc6f73036c0b8176cd52509c3b880a5526c
Magnus Giverin <magnusgiverin@icloud.com>
2024-09-06 12:04:07 +0200

    Add: Add appropriate colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,27 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["Noto", "Poppins", ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#24BD76",
+          dark: "#034E31",
+          light: "#C8EBDC",
+        },
+        secondary: {
+          black: "#2E2D30",
+          white: "#FFFFFF",
+          lightgray: "#F5F5F5",
+        },
+        background: {
+          light: "#CCE7FA",
+          blue: "#DDF3EB",
+          pink: "#FFD0D0",
+          purple: "#EBE8FE",
+          yellow: "#FFE9AE",
+          darkgreen: "#003C2D",
+        },
       },
     },
   },
