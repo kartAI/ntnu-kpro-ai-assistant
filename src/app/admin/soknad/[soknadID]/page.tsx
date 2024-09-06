88fdb95fdf0cedd3bdbb4f9a0ffb15299b29b25a
Magnus Giverin <magnusgiverin@icloud.com>
2024-09-06 11:20:29 +0200

    Add: Add T3 stack and page layout

---
+++ src/app/admin/soknad/[soknadID]/page.tsx
@@ -0,0 +1,10 @@
+
+export default async function AdminSoknad() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+          <p>{"Dette er siden for en admin og administrere en søknad"}</p>
+        </main>
+    );
+  }
+
+  
