9e5399fcbef2a23b074fc7cfc736797b4a9598c7
SverreNystad <sverrenystad@gmail.com>
2024-10-04 12:52:59 +0200

    fix: update pre-commit script to run linting in frontend directory

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Run linter on staged JavaScript, TypeScript files
-npx next lint --fix
+cd frontend
+npm run lint --fix
 
 if [ $? -ne 0 ]; then
   echo "Linting failed. Please fix the issues before committing."
