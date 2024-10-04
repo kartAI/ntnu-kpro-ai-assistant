e5f6d6a080d6a5dfb9f2c86784d56174a5a8f32d
SverreNystad <sverrenystad@gmail.com>
2024-10-04 12:52:46 +0200

    feat: update page title in tiltaksvisning

--- frontend/src/app/tiltaksvisning/page.tsx
+++ frontend/src/app/tiltaksvisning/page.tsx
@@ -4,6 +4,7 @@ import EmbeddedFrame from "../_components/EmbeddedFrame";
 export default async function Home() {
   return (
     <div>
+        <h1 data-cy="title">3D tiltaksvisning</h1>
         <EmbeddedFrame data-cy="tiltaksvisning"  src="https://byggesak3d.norkart.no/view/bf204afe-e50e-4ac6-8839-ebd9406167ac" title="3D tiltaksvisning" />
     </div>
   );
