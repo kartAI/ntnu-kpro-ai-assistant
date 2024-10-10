fd4ea4aa900d8cafb0faddecd232dba40f68293c
Sverre Nystad <sverrenystad@gmail.com>
2024-10-10 17:21:52 +0200

    feat: Add summarize endpoint to main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,26 @@
 from typing import Union
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class SummaryResponse(BaseModel):
+    summary: dict
+    cad_aid_summary: dict
+    arkivgpt_summary: dict
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
+@app.post("/summarize")
+def summarize(files: list[UploadFile], include_modules: bool = False) -> :
+    """
+    Summarize a file.
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    Args:
+        file (list[UploadFile]): The file to summarize.
+        include_modules (bool): Whether to include additional modules like CAD-AiD and ArkivGPT.
+    Returns:
+        ResponseSummary: The summarization response.
+
+    """
+    response = SummaryResponse(summary={}, cad_aid_summary={}, arkivgpt_summary={})
+    return response
