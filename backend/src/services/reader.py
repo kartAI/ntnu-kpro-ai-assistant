93dacb988ff407d5e920554fffd441f7c23b2e7d
Sverre Nystad <sverrenystad@gmail.com>
2024-10-11 11:56:33 +0200

    feat: Add reader service structure

---
+++ backend/src/services/reader.py
@@ -0,0 +1,6 @@
+from typing import Protocol
+from fastapi import UploadFile
+
+class Reader(Protocol):
+    def read(self, file: UploadFile) -> str:
+        ...
---
+++ backend/src/services/readers/pdf_reader.py
@@ -0,0 +1,9 @@
+from fastapi import UploadFile
+
+from src.services.reader import Reader
+
+class PdfReader(Reader):
+    def read(self, file: UploadFile) -> str:
+        # TODO USE fitz TO READ PDF FILES THAT ARE DIGITALIZED USE Tesseract TO READ PDF FILES THAT ARE SCANNED
+        text = ""
+        return text
