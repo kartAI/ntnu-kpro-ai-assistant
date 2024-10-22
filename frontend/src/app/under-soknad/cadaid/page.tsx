987735aa9566bf8a9865e949746fbc0ee484a44e
SverreNystad <sverrenystad@gmail.com>
2024-10-22 22:09:49 +0200

    feat: add CADAiD page to for-soknad and under-soknad

---
+++ frontend/src/app/under-soknad/cadaid/page.tsx
@@ -0,0 +1,10 @@
+import CadaidPage from "~/app/_components/CADAiD";
+
+export default async function PlantegningsAnalyse() {
+  return (
+    <div>
+        <CadaidPage></CadaidPage>
+    </div>
+  );
+}
+
