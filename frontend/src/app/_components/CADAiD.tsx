11a5a942eac5491f0b7e23e246828546757947bf
SverreNystad <sverrenystad@gmail.com>
2024-10-23 00:36:17 +0200

    feat: make cadaid mobile friendly

--- frontend/src/app/_components/CADAiD.tsx
+++ frontend/src/app/_components/CADAiD.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { FaCheckCircle, FaExclamationTriangle, FaTimesCircle } from 'react-icons/fa';
 import { Detection } from '../../types/detection';
-import { hasErrors, requiredDrawingTypes } from '../../utils/helpers';
 import FileList from './FileList';
 import UploadButton from './UploadButton';
 import Results from './Results';
@@ -67,19 +65,21 @@ const CadaidPage: React.FC = () => {
   };
 
   return (
-    <div className="p-6 min-h-screen flex flex-row">
-      <div className="w-1/3 pr-4">
-        <FileList files={files} onRemove={handleFileRemove} onUpload={handleFileUpload} />
+    <div className="p-6 min-h-screen flex flex-col md:flex-row">
+      {/* Left Column */}
+      <div className="w-full md:w-1/3 md:pr-4">
+        <FileList files={files} onRemove={handleFileRemove} onUpload={handleFileUpload}/>
         <Results results={results} />
       </div>
 
-      <div className="w-2/3">
+      {/* Right Column */}
+      <div className="w-full md:w-2/3">
         {/* File Preview */}
         <div className="mb-6 bg-white p-4 rounded-lg shadow-md h-full">
-          <div className="flex items-start ">
-            <h2 className="font-semibold text-lg mb-4">Forhåndsvisning av</h2>
+          <div className="flex flex-col sm:flex-row items-start">
+            <h2 className="font-semibold text-lg mb-4 sm:mb-0 sm:text-xl">Forhåndsvisning av</h2>
             <select
-              className="block border border-black bg-inherit rounded-lg mb-4 ml-4 text-gray-700"
+              className="block border border-black bg-inherit rounded-lg mb-4 sm:mb-0 sm:ml-4 text-gray-700 w-full sm:w-auto"
               onChange={(e) => handleSelectFile(e.target.value)}
               value={selectedFile?.name || ''}
               aria-label="Select file to preview"
