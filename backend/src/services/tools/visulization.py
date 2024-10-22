425cc2bf54112f4cad585a4285977db327ead8c0
SverreNystad <sverrenystad@gmail.com>
2024-10-22 14:57:51 +0200

    refactor: add type hinting to visualization

--- backend/src/services/tools/visulization.py
+++ backend/src/services/tools/visulization.py
@@ -1,7 +1,8 @@
-from IPython.display import Image, display
+from IPython.display import Image
+from langgraph.graph import StateGraph
 
 SAVE_PATH = "graph.png"
 
 
-def visualize(graph):
+def visualize(graph: StateGraph) -> None:
     Image(graph.get_graph().draw_mermaid_png()).save(SAVE_PATH)
