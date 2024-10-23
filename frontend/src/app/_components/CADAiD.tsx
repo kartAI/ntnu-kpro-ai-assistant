6002cfea9284db5b9b1f64acfeef00922f386431
Sverre Nystad <sverrenystad@gmail.com>
2024-10-23 09:17:51 +0200

    refactor: gather all of FilePreview component into FilePreview

--- frontend/src/app/_components/CADAiD.tsx
+++ frontend/src/app/_components/CADAiD.tsx
@@ -1,4 +1,3 @@
-// CadaidPage.tsx
 "use client";
 
 import React, { useState } from 'react';
@@ -23,7 +22,6 @@ async function fetchDetection(formData: FormData): Promise<Detection[]> {
 const CadaidPage: React.FC = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [results, setResults] = useState<Detection[]>([]);
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   /**
@@ -69,15 +67,6 @@ const CadaidPage: React.FC = () => {
     setResults((prevResults) => prevResults.filter(result => result.file_name !== fileName));
   };
 
-  /**
-   * Sets the selected file for preview.
-   * @param fileName - The name of the file to select.
-   */
-  const handleSelectFile = (fileName: string) => {
-    const file = files.find(file => file.name === fileName) ?? null;
-    setSelectedFile(file);
-  };
-
   return (
     <div className="p-6 min-h-screen flex flex-col md:flex-row" data-cy="main-container">
       {/* Left Column */}
@@ -98,29 +87,7 @@ const CadaidPage: React.FC = () => {
 
       {/* Right Column */}
       <div className="w-full md:w-2/3" data-cy="right-column">
-        {/* File Preview */}
-        <div className="mb-6 bg-white p-4 rounded-lg shadow-md h-full">
-          <div className="flex flex-col sm:flex-row items-start">
-            <h2 className="font-semibold text-lg mb-4 sm:mb-0 sm:text-xl">Forhåndsvisning av</h2>
-            <select
-              className="block border border-black bg-inherit rounded-lg mb-4 sm:mb-0 sm:ml-4 text-gray-700 w-full sm:w-auto"
-              onChange={(e) => handleSelectFile(e.target.value)}
-              value={selectedFile?.name ?? ''}
-              aria-label="Select file to preview"
-            >
-              <option value="">Velg fil å vise</option>
-              {files.map((file) => (
-                <option key={file.name} value={file.name}>
-                  {file.name}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          {selectedFile && (
-            <FilePreview selectedFile={selectedFile} />
-          )}
-        </div>
+        <FilePreview files={files} />
       </div>
     </div>
   );
