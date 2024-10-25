b3186a8a369bc087118ccb2cb8865f90fa09d643
maurice-wegerif <maurice.wegerif@gmail.com>
2024-10-25 13:13:00 +0200

    feat: add logging for unsupported filetype

--- backend/src/services/readers/factory.py
+++ backend/src/services/readers/factory.py
@@ -1,9 +1,12 @@
+import logging
+from fastapi import UploadFile
+
 from src.services.reader import Reader
 from src.services.readers.pdf_reader import PdfReader
 from src.services.readers.xml_reader import XmlReader
 from src.services.readers.ocr_reader import OCRReader
-from fastapi import UploadFile
 
+logger = logging.getLogger(__name__)
 
 def create_reader(file: UploadFile) -> Reader:
     match file.content_type:
@@ -17,6 +20,7 @@ def create_reader(file: UploadFile) -> Reader:
         case "application/xml":
             return XmlReader()
         case _:
+            logger.warning(f"Unsupported media type: {file.content_type} for file: {file.filename}")
             raise ValueError(f"Unsupported media type: {file.content_type}")
 
 def is_digital_file(file: UploadFile) -> bool:
