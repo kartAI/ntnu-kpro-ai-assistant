4753c9a21991281229c195b92fa3a81b4161b2a4
Sverre Nystad <sverrenystad@gmail.com>
2024-10-30 12:29:49 +0100

    fix: update CORS middleware to allow OPTIONS method

--- backend/src/main.py
+++ backend/src/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ORIGINS,
     allow_credentials=True,
-    allow_methods=["POST", "GET"],
+    allow_methods=["POST", "GET", "OPTIONS"],
     allow_headers=["*"],
 )
 
