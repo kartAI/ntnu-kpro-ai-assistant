333946bf1a079a98c42a19f2e2ed35d0852d8a77
Sverre Nystad <sverrenystad@gmail.com>
2024-11-06 13:54:24 +0100

    feat: create a common place for env variable retrieval

---
+++ backend/src/configuration.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ARKIVGPT_URL = os.getenv("ARKIVGPT_URL", "http://localhost:80/api")
+
+AZURE_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_API_BASE = os.getenv("AZURE_OPENAI_API_BASE")
+AZURE_API_VERSION = os.getenv("API_VERSION")
+AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+
+# TODO: add correct elements to the AzureOpenAI class
+API_KEY = os.getenv("OPENAI_API_KEY")
